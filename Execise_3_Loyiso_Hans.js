a6ee9d43314295972bc25db2598bf82e163e5c03
Rando <justtrando@gmail.com>
2025-10-29 15:19:29 +0200

    Make the layout more presentable

--- Execise_3_Loyiso_Hans.js
+++ Execise_3_Loyiso_Hans.js
@@ -1,26 +1,37 @@
 // TODO: Create an empty array called colors
+console.log("// TODO: Create an empty array called colors")
 
 let colors =  [];
 
+console.log("\nThis is the start of colors array ",colors)
+
 // TODO: Add three colors to the array using push()
+
+console.log("// TODO: Add three colors to the array using push()")
 
 colors.push("Blue");
 colors.push("Red");
 colors.push("Green");
 
+
+
 // TODO: Create another array called numbers with five numbers
+console.log("// TODO: Create another array called numbers with five numbers")
 
 const numbers = new Array(1,2,3,4,5);
 
 // TODO: Remove the last color from the colors array
+console.log("// TODO: Remove the last color from the colors array")
 
 colors.pop();
 
 // TODO: Add a new color to the beginning of the colors array
+console.log("// TODO: Add a new color to the beginning of the colors array")
 
 colors.unshift("Pink");
 
 // TODO: Print the length of both arrays
+console.log("// TODO: Print the length of both arrays")
 
 console.log(colors.length)
 console.log(numbers.length)
