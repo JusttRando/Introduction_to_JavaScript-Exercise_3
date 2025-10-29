e97c9e14662e3ac0ae57fdccfc3e291b78340e52
Rando <justtrando@gmail.com>
2025-10-29 15:08:52 +0200

    first commit

---
+++ Execise_3_Loyiso_Hans.js
@@ -0,0 +1,26 @@
+// TODO: Create an empty array called colors
+
+let colors =  [];
+
+// TODO: Add three colors to the array using push()
+
+colors.push("Blue");
+colors.push("Red");
+colors.push("Green");
+
+// TODO: Create another array called numbers with five numbers
+
+const numbers = new Array(1,2,3,4,5);
+
+// TODO: Remove the last color from the colors array
+
+colors.pop();
+
+// TODO: Add a new color to the beginning of the colors array
+
+colors.unshift("Pink");
+
+// TODO: Print the length of both arrays
+
+console.log(colors.length)
+console.log(numbers.length)
